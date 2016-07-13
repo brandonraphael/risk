bf51edd89fe1e3b764c5e569cf48373f82d2b484
brandonraphael <brandonraphael1@gmail.com>
2016-07-13 13:54:46 -0700

    Added game initializing functionality

--- public/javascripts/placePiece.js
+++ public/javascripts/placePiece.js
@@ -1,13 +1,24 @@
 function placePiece(territories, game, player) {
   var success = false;
-  territories.forEach(function(territory) {
+  territories.forEach(function(territory, idx, arr) {
     if (territory.phaserObj.contains(game.input.x, game.input.y) && game.input.activePointer.isDown) {
-      territory.updateOwner(player);
-      territory.updateInfantry(1);
-      success = true;
+      if (notAllTerritoresOwned(arr) && territory.owner === null) {
+        territory.updateOwner(player);
+        territory.updateInfantry(1);
+        success = true;
+      } else if (!notAllTerritoresOwned(arr) && territory.owner === game.turn) {
+        territory.updateInfantry(1);
+        success = true;
+      }
     }
   });
   return success;
 }
 
+function notAllTerritoresOwned(territories) {
+  return territories.reduce(function(prev, curr){
+    if (prev) return prev;
+    if (curr.owner === null) return true;
+  }, false)
+}
 module.exports = placePiece;
